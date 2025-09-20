072c674a2016ef029290a0215f5a0347f124fc5a
alliana-tenadu <allianaotenadu@gmail.com>
2025-09-20 22:33:13 +0000

    Refactor App.jsx for code consistency and formatting improvements

--- Clients/src/App.jsx
+++ Clients/src/App.jsx
@@ -1,50 +1,55 @@
-import "./App.css"
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, useParams, } from "react-router-dom"
-import locationPins from "../locations"
-import HomePage from "./Pages/HomePage"
-import WelcomePage from "./Pages/WelcomePage"
-import Map from "./Pages/Map"
-import WaveformPlayer from "./Components/WaveformPlayer"
-import ImagePreviewComponent from "./components/ImagePreviewComponent"
-import LyricsContainer from "./Components/LyricsContainer"
-import FilmReel from "./Components/FilmReel"
-import BTSPhotos from "./Components/BTSPhotos"
-import WrittenReflection from "./Components/WrittenReflection"
-import ErrorPage from "./Pages/ErrorPage"
-import VideoPlayer from "./components/VideoPlayer"
-import Lyricsbreakedown from "./Components/lyricsbreakedown"
+import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  useParams,
+} from "react-router-dom";
+import locationPins from "../locations";
+import HomePage from "./Pages/HomePage";
+import WelcomePage from "./Pages/WelcomePage";
+import Map from "./Pages/Map";
+import WaveformPlayer from "./components/WaveformPlayer";
+import ImagePreviewComponent from "./components/ImagePreviewComponent";
+import LyricsContainer from "./components/LyricsContainer";
+import FilmReel from "./components/FilmReel";
+import BTSPhotos from "./components/BTSPhotos";
+import WrittenReflection from "./components/WrittenReflection";
+import ErrorPage from "./Pages/ErrorPage";
+import VideoPlayer from "./components/VideoPlayer";
+import Lyricsbreakedown from "./components/lyricsbreakedown";
 
-function ContentRouter () {
+function ContentRouter() {
   const { id, contentType } = useParams();
   const location = locationPins.find((loc) => loc.id === Number(id));
-  
+
   if (!location) {
-    return <ErrorPage />
+    return <ErrorPage />;
   }
-  
+
   switch (contentType) {
     case "lyrics":
-      return <LyricsContainer />
-    case "audio": 
-      return <WaveformPlayer audioUrl={location.songUrl} />
+      return <LyricsContainer />;
+    case "audio":
+      return <WaveformPlayer audioUrl={location.songUrl} />;
     case "film-reel":
-      return <FilmReel videoUrl={location.videoUrl} />
+      return <FilmReel videoUrl={location.videoUrl} />;
     case "bts-photos":
-      return <BTSPhotos />
+      return <BTSPhotos />;
     case "reflection":
-      return <WrittenReflection />
+      return <WrittenReflection />;
     case "video":
-      return <VideoPlayer src={location.videoUrl} />
+      return <VideoPlayer src={location.videoUrl} />;
     case "voice-note":
-      return <WaveformPlayer audioUrl={location.songUrl} /> 
+      return <WaveformPlayer audioUrl={location.songUrl} />;
     case "mixing-notes":
-      return <ImagePreviewComponent /> 
+      return <ImagePreviewComponent />;
     case "interview":
-      return <VideoPlayer src={location.videoUrl} />   
+      return <VideoPlayer src={location.videoUrl} />;
     default:
       return <ErrorPage />;
   }
-
 }
 
 function App() {
@@ -56,16 +61,16 @@ function App() {
         <Route path="/welcome" element={<WelcomePage />} />
         <Route path="/map" element={<Map />} />
         <Route path="/location/:id/:contentType" element={<ContentRouter />} />
-        <Route path="*" element={<ErrorPage/>} />
-        <Route path="/lyrics" element={<Lyricsbreakedown/>} />
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/lyrics" element={<Lyricsbreakedown />} />
       </Route>
     )
-  )
+  );
   return (
     <div>
       <RouterProvider router={Router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
