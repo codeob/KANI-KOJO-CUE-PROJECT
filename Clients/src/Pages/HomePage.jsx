cd00005e3a3c4e8be4e5a08057761d69f66a4594
Derrode Walter Cheale <144121696+deeosy@users.noreply.github.com>
2025-09-23 11:52:06 +0000

    1

--- Clients/src/Pages/HomePage.jsx
+++ Clients/src/Pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Homepage from "../assets/homePage.jpg"
+import Homepage from "../assets/homepage.jpg"
 import { Link } from 'react-router-dom'
 import btn from '../assets/icons/button.svg'
 
