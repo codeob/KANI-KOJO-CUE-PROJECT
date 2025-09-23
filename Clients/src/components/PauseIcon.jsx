bc51e50e4337f3d3f0a71044c85e32809b6673b6
Derrode Walter Cheale <144121696+deeosy@users.noreply.github.com>
2025-09-23 12:03:23 +0000

    Rename Clients folder to client (fix case sensitivity)

--- Clients/src/components/PauseIcon.jsx
+++
@@ -1,9 +0,0 @@
-import pauseIcon from "../assets/icons/pause.svg"
-
-export default function PauseIcon() {
-  return (
-    <>
-    <img src={pauseIcon} alt="Pause Icon" className='h-[54px] w-[37px]' />
-    </>
-  )
-}
--- Clients/src/components/PlayIcon.jsx
+++
@@ -1,9 +0,0 @@
-import playIcon from "../assets/icons/play.svg"
-
-export default function PlayIcon() {
-  return (
-    <>
-    <img src={playIcon} alt="Play Icon" className='h-[44px] w-[37px]' />
-    </>
-  )
-}
