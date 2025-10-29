29bdc323fe305836d88e8e6b89be1788c3ddc634
Derrode Walter Cheale <144121696+deeosy@users.noreply.github.com>
2025-10-29 00:23:19 +0000

    1

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -12,7 +12,7 @@ function HomePage() {
         >
             {/* Title */}
              <div className='absolute top-10 sm:top-16 md:top-20 lg:top-24 xl:top-32 2xl:top-40'>
-              <img src={KaniLogo} alt="" className='w-[582.31px] sm:w-[582.31px] md:w-[582.31px] lg:w-[582.31px] lg:h-[162px]  md:h-[162px] xl:w-[650px] 2xl:w-[700px]' />
+              <img src={KaniLogo} alt="Kani Logo" className='w-[582.31px] sm:w-[582.31px] md:w-[582.31px] lg:w-[582.31px] lg:h-[162px]  md:h-[162px] xl:w-[650px] 2xl:w-[700px]' />
                 {/* Subtitle */}
             <p className="special-elite font-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl leading-relaxed text-center text-[#B69F7C] mt-2 sm:mt-3 md:mt-4 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-xl xl:max-w-2xl 2xl:max-w-3xl">
                 Step into the journey behind the music
