18c0543ae59b653a6f4fb7037becb3ecf622d032
Obed Tawiah <tobed1586@gmail.com>
2025-10-31 13:39:05 +0000

    done with welcomepage

--- client/src/Pages/WelcomePage.jsx
+++ client/src/Pages/WelcomePage.jsx
@@ -9,13 +9,13 @@ import contineFrame from '../assets/backgrounds/darkBrownFrame.svg';
 function WelcomePage() {
   return (
     <div
-      className="h-screen w-full bg-cover bg-center flex flex-col justify-center items-center px-4 relative"
+      className="h-screen w-full bg-cover bg-center flex flex-col justify-center items-center px-4 py-4 sm:py-6 md:py-8 lg:py-10 xl:py-12 relative"
       style={{ backgroundImage: `url(${mapimage})` }}
     >
-      <div className="absolute inset-0 bg-black/60 z-0"></div>
+      
       
       {/* Main Card Container */}
-      <div className="relative max-w-4xl w-full mx-auto z-10   rounded-xl"
+      <div className="relative max-w-4xl w-full mx-auto z-10 p-4 sm:p-5 md:p-6 lg:p-7 rounded-xl"
         style={{ backgroundImage: `url(${grayFrame})` }}
       >
         {/* Card with decorative border effect */}
@@ -24,19 +24,19 @@ function WelcomePage() {
           <img src={grainBG} alt="grain background" className="absolute  w-full h-full object-cover opacity-20 z-0" />
           
           {/* Content Container */}
-          <div className="relative z-10 p-4 sm:p-8 md:p-10 lg:p-12">
+          <div className="relative z-10 p-1 sm:p-2 md:p-3 lg:p-4">
             {/* Title */}
-            <div className="text-center mb-6 sm:mb-8">
-              <h1 className="text-[1.75rem] sm:text-[2rem] md:text-[2.25rem] lg:text-[2.5rem] rock text-primary-100 mb-2 leading-tight">
+            <div className="text-center mb-2 sm:mb-4">
+              <h1 className="text-sm sm:text-sm md:text-xl lg:text-2xl rock text-primary-100 mb-1 leading-tight ">
                 WELCOME TO KANI'S JOURNEY
               </h1>
             </div>
 
             {/* Map/Illustration Container */}
-            <img src={Frame} alt="" />
+            <img src={Frame} alt="" className="w-full h-auto object-contain " />
 
             {/* Description Text */}
-            <div className="text-center mb-6 sm:mb-8"> 
+            <div className="text-center mb-2 sm:mb-4"> 
               <p className="special-elite text-base sm:text-lg md:text-xl lg:text-2xl text-primary-100 leading-relaxed max-w-xl sm:max-w-2xl mx-auto"> 
                 Tap the red pins across the town to unlock the memories, moments, and music that shaped Kani's story.
               </p>
